47dd92966ec8956ad42b67364b55781a5946f6f4
Vadim Mychko <vadim_mychko@proton.me>
2024-05-03 07:05:36 +0200

    refactor hardware config for vivobook

--- hosts/vivobook/hardware-configuration.nix
+++ hosts/vivobook/hardware-configuration.nix
@@ -1,24 +1,22 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/f63500ff-803b-43fa-8776-2214d63cb967";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/f63500ff-803b-43fa-8776-2214d63cb967";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/EE97-69D8";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/EE97-69D8";
+    fsType = "vfat";
+  };
 
   swapDevices = [ ];
 
