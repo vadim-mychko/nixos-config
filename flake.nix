b4bf829b2ecb8eb8b15f585317aae8dfbc041b31
Vadim Mychko <vadim_mychko@proton.me>
2024-06-21 22:33:13 +0200

    stop using home manager

--- flake.nix
+++ flake.nix
@@ -3,11 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
@@ -15,14 +10,7 @@
       vivobook = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = inputs;
-        modules = [
-          ./hosts/vivobook/configuration.nix
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.mychkvad = import ./home/mychkvad.nix; 
-          }
-        ];
+        modules = [ ./hosts/vivobook/configuration.nix ];
       };
     };
   };
