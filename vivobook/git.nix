b19acf3642aed6626e9d1ba1ffee1c6542438723
Vadim Mychko <vadim_mychko@proton.me>
2024-06-22 15:50:53 +0200

    update the git nix file

--- vivobook/git.nix
+++ vivobook/git.nix
@@ -4,8 +4,10 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
-    user.name = "Vadim Mychko";
-    user.email = "vadim_mychko@proton.me";
-    init.defaultBranch = "main";
+    config = {
+      user.name = "Vadim Mychko";
+      user.email = "vadim_mychko@proton.me";
+      init.defaultBranch = "main";
+    };
   };
 }
