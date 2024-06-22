a1708378bd04a923a9a55de4ab4362c946171ee0
Vadim Mychko <vadim_mychko@proton.me>
2024-06-22 11:51:56 +0200

    remove home manager from the flake file

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations = {
       vivobook = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
