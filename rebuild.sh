9973a2fde1059db4b393e943944ca426255f446c
Vadim Mychko <vadim_mychko@proton.me>
2024-06-23 02:51:54 +0200

    use realpath for rebuilding script

--- rebuild.sh
+++ rebuild.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-sudo nixos-rebuild switch --flake $(dirname "$0")#vivobook
+
+SCRIPT_PATH=$(realpath "$0")
+SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+
+sudo nixos-rebuild switch --flake $SCRIPT_DIR#vivobook
