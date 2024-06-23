6500304886e978f0cddc5876d8b6e2c5586d9708
Vadim Mychko <vadim_mychko@proton.me>
2024-06-23 02:58:07 +0200

    force make symlinks

--- config/symlink.sh
+++ config/symlink.sh
@@ -3,6 +3,6 @@
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-ln -s $SCRIPT_DIR/foot $HOME/.config
-ln -s $SCRIPT_DIR/.zshrc $HOME
-ln -s $SCRIPT_DIR/.gitconfig $HOME
+ln -sf $SCRIPT_DIR/foot $HOME/.config
+ln -sf $SCRIPT_DIR/.zshrc $HOME
+ln -sf $SCRIPT_DIR/.gitconfig $HOME
