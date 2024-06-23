a7680df37a4ddc5260d7b752883accebc08ee132
Vadim Mychko <vadim_mychko@proton.me>
2024-06-23 15:43:18 +0200

    make the script more verbose

--- config/symlink.sh
+++ config/symlink.sh
@@ -4,7 +4,7 @@ SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 CONFIG_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 
-mkdir -p "$CONFIG_DIR"
+mkdir -pv "$CONFIG_DIR"
 ln -sfv "$SCRIPT_DIR/foot" "$CONFIG_DIR"
 ln -sfv "$SCRIPT_DIR/.zshrc" "$HOME"
 ln -sfv "$SCRIPT_DIR/.gitconfig" "$HOME"
