3853a64b85b2b342ff0eb61d1bbee8e608facad1
Vadim Mychko <vadim_mychko@proton.me>
2024-06-23 02:38:17 +0200

    move git settings to the dotfile

--- vivobook/git.nix
+++ vivobook/git.nix
@@ -4,10 +4,5 @@
   programs.git = {
     enable = true;
     lfs.enable = true;
-    config = {
-      user.name = "Vadim Mychko";
-      user.email = "vadim_mychko@proton.me";
-      init.defaultBranch = "main";
-    };
   };
 }
