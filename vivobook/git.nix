15d8e09924faef6351bcd20bb3f0b00f6d001087
Vadim Mychko <vadim_mychko@proton.me>
2024-06-23 17:32:25 +0200

    fix name

--- vivobook/git.nix
+++ vivobook/git.nix
@@ -6,7 +6,7 @@
     lfs.enable = true;
 
     config = {
-      user.name = "Vadim Myckho";
+      user.name = "Vadim Mychko";
       user.email = "vadim_mychko@proton.me";
       init.defaultBranch = "main";
     };
