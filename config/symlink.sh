729fb350ad1bbf5e243c6a5e895ddd5e6a15de1c
Vadim Mychko <vadim_mychko@proton.me>
2024-06-23 15:41:19 +0200

    update symlink bash script

--- config/symlink.sh
+++ config/symlink.sh
@@ -2,8 +2,9 @@
 
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+CONFIG_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 
-mkdir -p "$HOME/.config"
-ln -sfv "$SCRIPT_DIR/foot" "$HOME/.config"
+mkdir -p "$CONFIG_DIR"
+ln -sfv "$SCRIPT_DIR/foot" "$CONFIG_DIR"
 ln -sfv "$SCRIPT_DIR/.zshrc" "$HOME"
 ln -sfv "$SCRIPT_DIR/.gitconfig" "$HOME"
