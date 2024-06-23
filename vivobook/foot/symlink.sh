4a48093d4c225d2bf0a439cdce1073446b8e1f9b
Vadim Mychko <vadim_mychko@proton.me>
2024-06-23 18:38:11 +0200

    move foot configuration and symlinking to the vivobook/ directory

--- vivobook/foot/symlink.sh
+++ vivobook/foot/symlink.sh
@@ -5,6 +5,4 @@ SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 CONFIG_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 
 mkdir -pv "$CONFIG_DIR"
-ln -sfv "$SCRIPT_DIR/foot" "$CONFIG_DIR"
-ln -sfv "$SCRIPT_DIR/.zshrc" "$HOME"
-ln -sfv "$SCRIPT_DIR/.tmux.conf" "$HOME"
+ln -sfv "$SCRIPT_DIR" "$CONFIG_DIR"
