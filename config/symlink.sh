7439a461695c474cba3a7bf5faa745e2c29ff374
Vadim Mychko <vadim_mychko@proton.me>
2024-06-23 15:28:36 +0200

    make the symlink verbose & create the .config directory if needed

--- config/symlink.sh
+++ config/symlink.sh
@@ -3,6 +3,7 @@
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 
-ln -sf "$SCRIPT_DIR/foot" "$HOME/.config"
-ln -sf "$SCRIPT_DIR/.zshrc" "$HOME"
-ln -sf "$SCRIPT_DIR/.gitconfig" "$HOME"
+mkdir -p "$HOME/.config"
+ln -sfv "$SCRIPT_DIR/foot" "$HOME/.config"
+ln -sfv "$SCRIPT_DIR/.zshrc" "$HOME"
+ln -sfv "$SCRIPT_DIR/.gitconfig" "$HOME"
