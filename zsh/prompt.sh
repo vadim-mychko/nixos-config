57449b079735cde220d8773a8e8131be090ee2ac
Vadim Mychko <vadim_mychko@proton.me>
2024-07-11 18:15:41 +0200

    remove ugly star in the zsh prompt

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,5 +1,5 @@
 PROMPT='%{$fg_bold[blue]%}%~%{$reset_color%} $(git_prompt_info)'
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}✱"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
