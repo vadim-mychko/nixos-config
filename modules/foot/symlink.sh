eb4a50eaeedbfac5c4658a1c7ad835e6ec8e3028
Vadim Mychko <vadim_mychko@proton.me>
2024-07-12 13:50:52 +0200

    when symlinking, expose only configuration files instead of the whole directories

--- modules/foot/symlink.sh
+++ modules/foot/symlink.sh
@@ -3,6 +3,7 @@
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 CONFIG_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}"
+FOOT_CONFIG_DIR="$CONFIG_DIR/foot"
 
-mkdir -pv "$CONFIG_DIR"
-ln -sfv "$SCRIPT_DIR" "$CONFIG_DIR"
+mkdir -pv "$FOOT_CONFIG_DIR"
+ln -sfv "$SCRIPT_DIR/foot.ini" "$FOOT_CONFIG_DIR"
--- modules/sway/symlink.sh
+++ modules/sway/symlink.sh
@@ -3,6 +3,7 @@
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 CONFIG_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}"
+SWAY_CONFIG_DIR="$CONFIG_DIR/sway"
 
-mkdir -pv "$CONFIG_DIR"
-ln -sfv "$SCRIPT_DIR" "$CONFIG_DIR"
+mkdir -pv "$SWAY_CONFIG_DIR"
+ln -sfv "$SCRIPT_DIR/config" "$SWAY_CONFIG_DIR"
