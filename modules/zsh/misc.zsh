e8efbb876dfd32f2685c016d3c44ecd2f240129c
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 20:36:27 +0200

    return vim mode

--- modules/zsh/misc.zsh
+++ modules/zsh/misc.zsh
@@ -1,2 +1,7 @@
+# ================================= VIM MODE ==================================
+bindkey -v
+export KEYTIMEOUT=1
+
+# ============================== INITIALIZATION ===============================
 source <(fzf --zsh)
 eval "$(zoxide init zsh)"
