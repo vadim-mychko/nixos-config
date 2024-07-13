466915af222f18541cf73c2807c66fff50285ce7
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 20:09:53 +0200

    add vim mode

--- modules/zsh/misc.zsh
+++ modules/zsh/misc.zsh
@@ -4,6 +4,10 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 zstyle ':completion:*' menu select
 _comp_options+=(globdots)
 
+# ================================= VIM MODE ==================================
+bindkey -v
+export KEYTIMEOUT=1
+
 # ============================== INITIALIZATION ===============================
 source <(fzf --zsh)
 eval "$(zoxide init zsh)"
