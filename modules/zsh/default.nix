4bd201f44c680f3292b3b3b9fe14a503131438b0
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 18:54:35 +0200

    add misc zsh file

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -12,11 +12,13 @@
       highlightStyle = "fg=15";
     };
 
+    shellAliases = null;
     interactiveShellInit = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       ${builtins.readFile ./prompt.zsh}
       ${builtins.readFile ./aliases.zsh}
       ${builtins.readFile ./variables.zsh}
+      ${builtins.readFile ./misc.zsh}
     '';
   };
 }
