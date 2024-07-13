4e27dbbab71014cec37d42801585ade558f4a164
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 15:38:51 +0200

    return nvidia power management, fixes the problem

--- hosts/vivobook/configuration.nix
+++ hosts/vivobook/configuration.nix
@@ -99,12 +99,13 @@
   };
 
   boot.initrd.kernelModules = [ "xe" ];
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
+    powerManagement.enable = true;
+    powerManagement.finegrained = true;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
