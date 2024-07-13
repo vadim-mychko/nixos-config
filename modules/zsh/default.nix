601c5cef69ae6db772fbac1d1e77c093586aca84
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 18:51:00 +0200

    fix

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -12,19 +12,11 @@
       highlightStyle = "fg=15";
     };
 
-    # 1. interactiveShellInit |
-    # 2. shellAliases         |
-    # 3. promptInit           v
-
     interactiveShellInit = ''
-      ${builtins.readFile ./variables.zsh}
-    '';
-
-    shellAliases = builtins.readFile ./aliases.zsh;
-
-    promptInit = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       ${builtins.readFile ./prompt.zsh}
+      ${builtins.readFile ./aliases.zsh}
+      ${builtins.readFile ./variables.zsh}
     '';
   };
 }
