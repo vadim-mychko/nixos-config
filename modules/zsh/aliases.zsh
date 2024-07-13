0debdbcc70c258956df36bd680675db740c7fa70
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 20:07:42 +0200

    configure auto completion

--- modules/zsh/aliases.zsh
+++ modules/zsh/aliases.zsh
@@ -2,3 +2,5 @@ alias vi='nvim'
 alias vim='nvim'
 alias vimdiff='nvim -d'
 alias cd=z
+alias find=fd
+alias grep=rg
--- modules/zsh/misc.zsh
+++ modules/zsh/misc.zsh
@@ -1,7 +1,9 @@
+# ============================== AUTO COMPLETION ==============================
 autoload -Uz compinit && compinit
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' menu select
+_comp_options+=(globdots)
 
-# =========================== BEGIN INITIALIZATION ============================
+# ============================== INITIALIZATION ===============================
 source <(fzf --zsh)
 eval "$(zoxide init zsh)"
-# ============================ END INITIALIZATION =============================
