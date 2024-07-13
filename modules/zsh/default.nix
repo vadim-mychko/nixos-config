abf1d2ffe43455ace4d3e3a3bc0417bda9dc5a6b
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 20:38:25 +0200

    remove fzf tab for now

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -4,7 +4,6 @@
   users.defaultUserShell = pkgs.zsh;
   environment.systemPackages = with pkgs; [
     zsh-powerlevel10k
-    zsh-fzf-tab
   ];
 
   programs.zsh = {
@@ -12,7 +11,6 @@
     histSize = 10000;
     interactiveShellInit = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
       ${builtins.readFile ./prompt.zsh}
       ${builtins.readFile ./aliases.zsh}
       ${builtins.readFile ./variables.zsh}
