2e5b49d41459a22f141039469d592a83b7fe7ecc
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 22:36:26 +0200

    add fzf tab

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -4,13 +4,14 @@
   users.defaultUserShell = pkgs.zsh;
   environment.systemPackages = with pkgs; [
     zsh-powerlevel10k
+    zsh-fzf-tab
   ];
 
   programs.zsh = {
     enable = true;
-    histSize = 10000;
     interactiveShellInit = ''
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
       ${builtins.readFile ./prompt.zsh}
       ${builtins.readFile ./aliases.zsh}
       ${builtins.readFile ./variables.zsh}
--- modules/zsh/misc.zsh
+++ modules/zsh/misc.zsh
@@ -1,12 +1,28 @@
+# https://youtu.be/ud7YxC33Z3w?si=0XJlozgWevORbcph
 # ============================== AUTO COMPLETION ==============================
-zstyle ':completion:*' matcher-list 'r:|?=** m:{a-z\-}={A-Z\_}'
-zstyle ':completion:*' menu select
-_comp_options+=(globdots)
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
+zstyle ':completion:*' menu no
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
+zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
+
+# ================================== HISTORY ==================================
+HISTSIZE=10000
+SAVEHIST=10000
+HISTFILE=$HOME/.zsh_history
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt hist_ignore_space
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
 
 # ================================= VIM MODE ==================================
 bindkey -v
 export KEYTIMEOUT=1
 
 # ============================== INITIALIZATION ===============================
-source <(fzf --zsh)
-eval "$(zoxide init zsh)"
+eval "$(fzf --zsh)"
+eval "$(zoxide init --cmd cd zsh)"
