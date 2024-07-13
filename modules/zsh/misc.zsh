3bf9511fbd97fd10a87ce9bb7e770c5134ce5f71
Vadim Mychko <vadim_mychko@proton.me>
2024-07-13 20:36:02 +0200

    add fzf tab plugin

--- modules/zsh/misc.zsh
+++ modules/zsh/misc.zsh
@@ -1,11 +1,2 @@
-# ============================== AUTO COMPLETION ==============================
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-zstyle ':completion:*' menu select
-
-# ================================= VIM MODE ==================================
-bindkey -v
-export KEYTIMEOUT=1
-
-# ============================== INITIALIZATION ===============================
 source <(fzf --zsh)
 eval "$(zoxide init zsh)"
