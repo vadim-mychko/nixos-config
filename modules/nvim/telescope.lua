6fcbda85b89ffbc9e7c091be2ad070981990e0a3
Vadim Mychko <vadim_mychko@proton.me>
2024-07-14 18:29:32 +0200

    minor refactor

--- modules/nvim/telescope.lua
+++ modules/nvim/telescope.lua
@@ -1,5 +1,6 @@
 -- https://youtu.be/m8C0Cq9Uv9o?si=3xxD7fEK63qZ41PO
 -- https://github.com/nvim-lua/kickstart.nvim/
+
 local telescope = require("telescope")
 telescope.setup {}
 telescope.load_extension("fzf")
