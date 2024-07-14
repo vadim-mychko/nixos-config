f0cc90fc649e3dbd2e744aee01d8fb6522792668
Vadim Mychko <vadim_mychko@proton.me>
2024-07-14 16:20:23 +0200

    minor changes to the nvim config

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -3,13 +3,13 @@
 {
   programs.neovim = {
     enable = true;
+    configure.packages.myVimPackage.start = with pkgs.vimPlugins; [
+      modus-themes-nvim
+    ];
+
     configure.customRC = ''
       lua << EOF
       ${builtins.readFile ./theme.lua}
     '';
-
-    configure.packages.myVimPackage.start = with pkgs.vimPlugins; [
-      modus-themes-nvim
-    ];
   };
 }
