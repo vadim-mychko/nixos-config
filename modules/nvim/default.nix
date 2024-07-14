783ab496b8e99950807e9f4848c6e0e28860d266
Vadim Mychko <vadim_mychko@proton.me>
2024-07-14 17:34:44 +0200

    add lsp, set more options, install python/nix language servers

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,12 +1,18 @@
 { pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    pyright
+    nil
+  ];
+
   programs.neovim = {
     enable = true;
     configure.packages.myVimPackage.start = with pkgs.vimPlugins; [
       modus-themes-nvim
       nvim-treesitter.withAllGrammars
       telescope-nvim
+      nvim-lspconfig
     ];
 
     configure.customRC = ''
@@ -14,6 +20,7 @@
       ${builtins.readFile ./options.lua}
       ${builtins.readFile ./mappings.lua}
       ${builtins.readFile ./theme.lua}
+      ${builtins.readFile ./lsp.lua}
     '';
   };
 }
--- modules/nvim/options.lua
+++ modules/nvim/options.lua
@@ -1,4 +1,17 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
 
 vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.breakindent = true
+vim.opt.signcolumn = "yes"
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.guicursor = ""
+vim.opt.showmode = false
+vim.opt.mouse = "a"
