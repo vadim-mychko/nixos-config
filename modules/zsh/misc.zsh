58ede79442a259661fae4c0b3823ccd3ddd2111f
Vadim Mychko <vadim_mychko@proton.me>
2024-07-14 14:43:40 +0200

    add left autocompletion also

--- modules/zsh/misc.zsh
+++ modules/zsh/misc.zsh
@@ -1,6 +1,8 @@
-# https://youtu.be/ud7YxC33Z3w?si=0XJlozgWevORbcph
+# [1] https://youtu.be/ud7YxC33Z3w?si=0XJlozgWevORbcph
+# [2] https://stackoverflow.com/a/24237590
+# [3] https://unix.stackexchange.com/a/97844
 # ============================== AUTO COMPLETION ==============================
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}' 'l:|=* r:|=*'
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' menu no
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'ls --color $realpath'
@@ -19,6 +21,8 @@ setopt hist_ignore_all_dups
 setopt hist_save_no_dups
 setopt hist_ignore_dups
 setopt hist_find_no_dups
+bindkey '^[[A' history-beginning-search-backward
+bindkey '^[[B' history-beginning-search-forward
 
 # ================================= VIM MODE ==================================
 bindkey -v
