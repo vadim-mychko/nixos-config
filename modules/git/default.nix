2c1b7e970265484b17d14abc7a635369412515fc
Vadim Mychko <vadim_mychko@proton.me>
2024-07-20 19:55:07 +0200

    use nvimdiff as a difftool

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -8,6 +8,8 @@
       user.name = "Vadim Mychko";
       user.email = "vadim_mychko@proton.me";
       init.defaultBranch = "main";
+      diff.tool = "nvimdiff";
+      difftool.prompt = "false";
     };
   };
 }
