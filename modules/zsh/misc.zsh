a11bc651e1c10bef6991ea86653127522e9e8494
Vadim Mychko <vadim_mychko@proton.me>
2024-07-20 19:43:00 +0200

    small refactor

--- modules/zsh/misc.zsh
+++ modules/zsh/misc.zsh
@@ -25,11 +25,13 @@ zmodload zsh/terminfo
 autoload -U history-search-end
 zle -N history-beginning-search-backward-end history-search-end
 zle -N history-beginning-search-forward-end history-search-end
+
 bindkey "$terminfo[kcuu1]" history-beginning-search-backward-end
-bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
 bindkey "^[[A" history-beginning-search-backward-end
-bindkey "^[[B" history-beginning-search-forward-end
 bindkey "^[OA" history-beginning-search-backward-end
+
+bindkey "$terminfo[kcud1]" history-beginning-search-forward-end
+bindkey "^[[B" history-beginning-search-forward-end
 bindkey "^[OB" history-beginning-search-forward-end
 
 # ================================= VIM MODE ==================================
