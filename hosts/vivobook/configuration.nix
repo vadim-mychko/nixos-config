ec8c3c00fccb1c98865b947d9b263c1f86c99f3d
Vadim Mychko <vadim_mychko@proton.me>
2024-09-04 12:30:22 +0200

    use open kernel nvidia drivers

--- hosts/vivobook/configuration.nix
+++ hosts/vivobook/configuration.nix
@@ -107,7 +107,7 @@
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = true;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     prime = {
