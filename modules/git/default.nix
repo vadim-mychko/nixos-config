0cc053e4cd55f7a880458adf4db24589d943ea7e
Vadim Mychko <vadim_mychko@proton.me>
2024-09-17 18:19:24 +0200

    update color theme of the delta pager

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -6,15 +6,29 @@
     lfs.enable = true;
     config = {
       user.name = "Vadim Mychko";
-      user.email = "vadim_mychko@proton.me";
+      user.email = "vadim-mychko@proton.me";
       init.defaultBranch = "main";
-      core.pager = "delta";
-      interactive.diffFilter = "delta --color-only";
-      delta.navigate = "true";
-      merge.conflictstyle = "diff3";
-      diff.colorMoved = "default";
       diff.tool = "nvimdiff";
       difftool.prompt = "false";
+
+      core.pager = "delta";
+      interactive.diffFilter = "delta --color-only";
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
+      delta = {
+        navigate = true;
+        "minus-style" = "syntax \"#ffbfbf\"";
+        "minus-non-emph-style" = "syntax \"#ffbfbf\"";
+        "minus-emph-style" = "syntax \"#ffbfbf\"";
+        "minus-empty-line-marker-style" = "syntax \"#ffbfbf\"";
+        "line-numbers-minus-style" = "#ffbfbf";
+        "plus-style" = "syntax \"#a0e0a0\"";
+        "plus-non-emph-style" = "syntax \"#a0e0a0\"";
+        "plus-emph-style" = "syntax \"#a0e0a0\"";
+        "plus-empty-line-marker-style" = "syntax \"#a0e0a0\"";
+        "line-numbers-plus-style" = "#a0e0a0";
+        "line-numbers-zero-style" = "#989898";
+      };
     };
   };
 }
