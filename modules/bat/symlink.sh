7748b7321f71fe351eecbedb9e2d7b49feaaeb36
Vadim Mychko <vadim_mychko@proton.me>
2024-09-17 16:36:02 +0200

    build the themes by default

--- modules/bat/symlink.sh
+++ modules/bat/symlink.sh
@@ -9,3 +9,4 @@ mkdir -pv "$BAT_CONFIG_DIR"
 mkdir -pv "$BAT_CONFIG_DIR/themes"
 ln -sv "$SCRIPT_DIR/config" "$BAT_CONFIG_DIR"
 ln -sv "$SCRIPT_DIR/modus_vivendi.tmTheme" "$BAT_CONFIG_DIR/themes"
+bat cache --build
