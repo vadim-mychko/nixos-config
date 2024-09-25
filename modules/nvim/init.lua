cc49794bbe5dde331b85f2badbf4ea605c71c388
Vadim Mychko <vadim_mychko@proton.me>
2024-09-26 01:13:42 +0200

    add session restore

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -67,6 +67,7 @@ dashboard.section.buttons.val = {
   dashboard.button( "e", "  > New file" , "<cmd>ene<CR>"),
   dashboard.button("SPC sf", "  > [S]earch [F]ile", "<cmd>Telescope find_files<CR>"),
   dashboard.button("SPC ss", "  > [S]earch [S]tring", "<cmd>Telescope live_grep<CR>"),
+  dashboard.button("SPC wr", "󰁯  > [W]orkspace [R]estore", "<cmd>SessionRestore<CR>"),
   dashboard.button("q", "  > Quit Neovim", "<cmd>qa<CR>"),
 }
 
