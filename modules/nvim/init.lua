56e8c1d4f587a01d33a36ea071e8a3efc4aa4c2a
Vadim Mychko <vadim_mychko@proton.me>
2024-09-26 22:11:02 +0200

    add more keymp

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -280,3 +280,4 @@ vim.keymap.set("n", "<leader>hn", gitsigns.next_hunk, { desc = "[H]unk [N]ext" }
 vim.keymap.set("n", "<leader>hN", gitsigns.prev_hunk, { desc = "[H]unk [N]ext backwards" })
 vim.keymap.set("n", "<leader>hS", gitsigns.stage_buffer, { desc = "[H]unk [S]tage buffer" })
 vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer, { desc = "[H]unk [R]eset buffer" })
+vim.keymap.set("n", "<leader>hU", gitsigns.reset_buffer_index, { desc = "[H]unk [R]estore buffer" })
