19acb6ce0274203cc696bc5b38c8bd690941500b
Vadim Mychko <vadim_mychko@proton.me>
2024-09-29 20:56:19 +0200

    upd wezterm config

--- modules/wezterm/symlink.sh
+++ modules/wezterm/symlink.sh
@@ -6,4 +6,6 @@ CONFIG_DIR="${XDG_CONFIG_HOME:-"$HOME/.config"}"
 WEZTERM_CONFIG_DIR="$CONFIG_DIR/wezterm"
 
 mkdir -pv "$WEZTERM_CONFIG_DIR"
+mkdir -pv "$WEZTERM_CONFIG_DIR/colors"
 ln -sv "$SCRIPT_DIR/wezterm.lua" "$WEZTERM_CONFIG_DIR"
+ln -sv "$SCRIPT_DIR/modus_vivendi.toml" "$WEZTERM_CONFIG_DIR/colors"
--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -1,13 +1,42 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local _, _, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
 
 config.window_decorations = "RESIZE"
-config.color_scheme = "Modus-Vivendi"
-config.font_size = 12.0
+config.color_scheme = "Modus Vivendi"
+config.font_size = 14.0
 config.font = wezterm.font_with_fallback({
   "JetBrains Mono NL",
   "MesloLGS NF",
   "Noto Color Emoji",
 })
+
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+  { key = "n",          mods = "CTRL", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+  { key = ";",          mods = "LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+  { key = "'",          mods = "LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+  { key = "z",          mods = "LEADER", action="TogglePaneZoomState" },
+  { key = "1",          mods = "LEADER", action=wezterm.action{ActivateTab=0}},
+  { key = "2",          mods = "LEADER", action=wezterm.action{ActivateTab=1}},
+  { key = "3",          mods = "LEADER", action=wezterm.action{ActivateTab=2}},
+  { key = "4",          mods = "LEADER", action=wezterm.action{ActivateTab=3}},
+  { key = "5",          mods = "LEADER", action=wezterm.action{ActivateTab=4}},
+  { key = "6",          mods = "LEADER", action=wezterm.action{ActivateTab=5}},
+  { key = "7",          mods = "LEADER", action=wezterm.action{ActivateTab=6}},
+  { key = "8",          mods = "LEADER", action=wezterm.action{ActivateTab=7}},
+  { key = "9",          mods = "LEADER", action=wezterm.action{ActivateTab=8}},
+  { key = "LeftArrow",  mods = "ALT",    action=wezterm.action{ActivatePaneDirection="Left"}},
+  { key = "DownArrow",  mods = "ALT",    action=wezterm.action{ActivatePaneDirection="Down"}},
+  { key = "UpArrow",    mods = "ALT",    action=wezterm.action{ActivatePaneDirection="Up"}},
+  { key = "RightArrow", mods = "ALT",    action=wezterm.action{ActivatePaneDirection="Right"}},
+  { key = "LeftArrow",  mods = "SHIFT",  action=wezterm.action{ActivateTabRelative=-1}},
+  { key = "RightArrow", mods = "SHIFT",  action=wezterm.action{ActivateTabRelative=1}},
+}
 
 return config
