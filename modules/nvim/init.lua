89bb43e1d28f7aae0c7650021e23279447f4152b
Vadim Mychko <vadim_mychko@proton.me>
2024-09-30 15:26:07 +0200

    upd

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -386,7 +386,6 @@ vim.g.molten_auto_open_output = false
 vim.g.molten_wrap_output = true
 vim.g.molten_virt_text_output = true
 vim.g.molten_virt_lines_off_by_1 = true
-vim.g.molten_virt_text_max_lines = 100
 
 require("wezterm").setup({})
 
