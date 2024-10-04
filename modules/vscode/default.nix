46fa2be6f8933e803f1ea37ec3e5997155f684b6
Vadim Mychko <vadim_mychko@proton.me>
2024-10-04 17:32:10 +0200

    upd basedpyright

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 
 let
-  basedpyright = pkgs.vscode-utils.extensionsFromVscodeMarketplace {
+  basedpyright = pkgs.vscode-utils.extensionFromVscodeMarketplace {
     name = "basedpyright";
     publisher = "detachhead";
     version = "1.18.3";
-    sha256 = "";
+    sha256 = "sha256-zOYxcw2NnIaIrdP2/xg5uIvGynmWpljTof/F+J/EXis=";
   };
   vscode = pkgs.vscode-with-extensions.override {
     vscodeExtensions = with pkgs.vscode-extensions; [
