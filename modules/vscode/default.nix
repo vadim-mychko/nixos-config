af7beffb691ca2dc5af5217512b96fd327bc2a72
Vadim Mychko <vadim_mychko@proton.me>
2024-10-06 14:37:32 +0200

    add quarto/vim for vscode

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -1,14 +1,22 @@
 { pkgs, ... }:
 
 let
+  quarto = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "quarto";
+    publisher = "quarto";
+    version = "1.115.0";
+    sha256 = "sha256-LTgeVDZSgwK/m4sXbVynyXnPnni41jbFAyxkhh0WBKY=";
+  };
   vscode = pkgs.vscode-with-extensions.override {
     vscodeExtensions = with pkgs.vscode-extensions; [
+      quarto
       mkhl.direnv
       charliermarsh.ruff
       ms-toolsai.jupyter
       valentjn.vscode-ltex
       ms-python.python
       ms-python.vscode-pylance
+      vscodevim.vim
     ];
   };
 in {
