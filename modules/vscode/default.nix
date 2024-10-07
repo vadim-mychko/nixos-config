c07426be3164f36a112c285f122f118ce664a4c1
Vadim Mychko <vadim_mychko@proton.me>
2024-10-07 17:14:27 +0200

    add more extensions

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -7,15 +7,26 @@ let
     version = "1.115.0";
     sha256 = "sha256-LTgeVDZSgwK/m4sXbVynyXnPnni41jbFAyxkhh0WBKY=";
   };
+  python-indent = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "vsc-python-indent";
+    publisher = "KevinRose";
+    version = "1.18.0";
+    sha256 = "";
+  };
   vscode = pkgs.vscode-with-extensions.override {
     vscodeExtensions = with pkgs.vscode-extensions; [
       quarto
+      python-indent
       mkhl.direnv
       charliermarsh.ruff
       ms-toolsai.jupyter
+      ms-toolsai.jupyter-keymap
+      ms-toolsai.jupyter-renderers
+      ms-toolsai.datawrangler
       valentjn.vscode-ltex
       ms-python.python
       ms-python.vscode-pylance
+      njpwerner.autodocstring
       vscodevim.vim
       reditorsupport.r
       usernamehw.errorlens
