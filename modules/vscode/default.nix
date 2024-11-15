af57256fc122ee3793cf5e7716f839cb11662eb4
Vadim Mychko <vadim-mychko@proton.me>
2024-11-15 14:05:12 +0100

    add edit csv for vscode

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -15,10 +15,18 @@ let
     sha256 = "sha256-hiOMcHiW8KFmau7WYli0pFszBBkb6HphZsz+QT5vHv0=";
   };
 
+  edit-csv = pkgs.vscode-utils.extensionFromVscodeMarketplace {
+    name = "vscode-edit-csv";
+    publisher = "janisdd";
+    version = "0.10.0";
+    sha256 = "sha256-+sdlEpBpsy4l4wL1gi+ONCi5de9mX4eP85HC9dsuc5Q=";
+  };
+
   vscode = pkgs.vscode-with-extensions.override {
     vscodeExtensions = with pkgs.vscode-extensions; [
       quarto
       python-indent
+      edit-csv
       mkhl.direnv
       charliermarsh.ruff
       ms-toolsai.jupyter
