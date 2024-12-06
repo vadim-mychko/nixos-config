9a3e005cc35b0bf42b341eb656a24cff15bcbac6
Vadim Mychko <vadim-mychko@proton.me>
2024-12-06 11:54:39 +0100

    remap defaults for slime

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -402,7 +402,12 @@ require("virt-column").setup({
 
 -- =================================== REPL ===================================
 vim.g.slime_target = "wezterm"
+vim.g.slime_no_mappings = 1
 vim.g.slime_bracketed_paste = 1
 vim.g.slime_default_config = {
   pane_direction = "right",
 }
+
+vim.keymap.set("n", "<leader>rl", "<Plug>SlimeLineSend", { desc = "[R]un [L]ine"})
+vim.keymap.set("v", "<leader>rl", "<Plug>SlimeRegionSend", { desc = "[R]un [L]ines (visual)"})
+vim.keymap.set("n", "<leader>r", "<Plug>SlimeMotionSend", { desc = "[R]un + motion"})
