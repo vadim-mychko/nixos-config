b02e5eeff5f36de6457bd9bee5f64b65f7d1a310
Vadim Mychko <vadim-mychko@proton.me>
2025-01-14 14:36:10 +0100

    add devshell

--- flake.nix
+++ flake.nix
@@ -5,13 +5,23 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, ... }@inputs: {
+  outputs = { nixpkgs, ... }@inputs:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
     nixosConfigurations = {
       vivobook = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        system = system;
         specialArgs = { inherit inputs; };
         modules = [ ./hosts/vivobook/configuration.nix ];
       };
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        python312
+      ];
     };
   };
 }
