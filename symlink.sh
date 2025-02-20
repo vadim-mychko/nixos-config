e96dd8d86667faf718b223d21f45c3482bc58071
Vadim Mychko <vadim-mychko@proton.me>
2025-02-20 11:53:10 +0100

    rename bash scripts, remove .sh (shell)

--- symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_PATH=$(realpath "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-
-"$SCRIPT_DIR/modules/bat/symlink.sh"
-"$SCRIPT_DIR/modules/git/symlink.sh"
-"$SCRIPT_DIR/modules/nvim/symlink.sh"
-"$SCRIPT_DIR/modules/wezterm/symlink.sh"
